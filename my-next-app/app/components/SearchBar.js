868f6962feaf5c840b5918e343b8fb06499914c6
DongHyunShin <personarpg7501@gmail.com>
2024-01-02 15:21:59 +0900

    check : search-bar의 스타일 부여

--- my-next-app/app/components/SearchBar.js
+++ my-next-app/app/components/SearchBar.js
@@ -2,9 +2,12 @@
 import React from 'react';
 
 const SearchBar = ({ onSearch }) => (
-    <div id="search-bar">
+    <div id="search-bar" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }}>
         <input type="text" id="search-input" placeholder="Write your name." />
-        <button id="show-button" onClick={onSearch}>
+        <button
+            id="show-button"
+            onClick={onSearch}
+        >
             Show
         </button>
     </div>
