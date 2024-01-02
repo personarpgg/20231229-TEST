751ed6d4ec4d37d060d4cebc5cc2ade31d0d329b
DongHyunShin <personarpg7501@gmail.com>
2024-01-02 15:22:31 +0900

    check : 왼쪽 메뉴에서 학생을 클릭 할 경우 '■' 가 표시되도록 설정

--- my-next-app/app/components/StudentList.js
+++ my-next-app/app/components/StudentList.js
@@ -1,14 +1,26 @@
 // components/StudentList.js
+
 import React from 'react';
 
-const StudentList = ({ students, onSelectStudent }) => (
-    <ul className="student-list" onClick={onSelectStudent}>
-        {students.map((student, index) => (
-            <li key={index} className="student-list-item">
-                {student}
-            </li>
-        ))}
+const StudentList = ({ students, onSelectStudent, selectedStudent }) => {
+  const handleSelectStudent = (student) => {
+    console.log('Selected student:', student);
+    onSelectStudent(student);
+  };
+
+  return (
+    <ul className="student-list">
+      {students.map((student, index) => (
+        <li
+          key={index}
+          className={`student-list-item${selectedStudent === student ? ' selected' : ''}`}
+          onClick={() => handleSelectStudent(student)}
+        >
+          {selectedStudent === student && '■'} {student}
+        </li>
+      ))}
     </ul>
-);
+  );
+};
 
 export default StudentList;
